9f90c9fe217794a27e9d0c5f701556baace1b83d
shocker wsl <fmju96@gmail.com>
2021-11-16 21:30:39 -0300

    Update cruz-verde scaper

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -6,26 +6,26 @@ interface Price {
   sale?: number;
 }
 
-const scrapers = {
+interface Scrapers {
+  [pharmacy: string]: (document: HTMLElement) => Price;
+}
+
+const scrapers: Scrapers = {
   'cruz-verde': (document: HTMLElement): Price => {
-    const data = document.querySelectorAll('div.price span.value');
+    const data = document.querySelector('script[type="application/ld+json"]');
     if (!data) throw new Error('Coldn\'t parse price from Cruz Verde');
-    const saleString = data[0].getAttribute('content');
-    const normalString = data[1].getAttribute('content');
-    const price: Price = {};
-    if (saleString) price.sale = parseFloat(saleString);
-    if (normalString) price.normal = parseFloat(normalString);
+    const price = JSON.parse(data.innerHTML);
     return price;
   },
 };
 
-export const scrap = async (url: string) => {
+export const scrap = async (pharmacySlug: string, url: string) => {
   const response = await axios(url);
 
   const document = parse(response.data);
 
-  console.log(scrapers['cruz-verde'](document));
+  console.log(scrapers[pharmacySlug](document));
 
 };
 
-scrap('https://www.cruzverde.cl/neohysticlar-desloratadina-5-mg-30-comprimidos-recubierto/251267.html');
+scrap('cruz-verde', 'https://www.cruzverde.cl/neohysticlar-desloratadina-5-mg-30-comprimidos-recubierto/251267.html');
